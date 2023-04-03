de072b561742364d711f751d3752d874dc773734
Charles <charleslanop@gmail.com>
2023-04-03 20:38:32 -0300

    update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,82 @@
+import 'package:creatures_online_client/utils/data_image.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = PageController(initialPage: 0);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: controller,
+          onPageChanged: (int index) {
+            print('Page ${index + 1}');
+          },
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(areaBootcamp1),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: const Center(
+                child: Text('Page 1'),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(areaBootcamp2),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: const Center(
+                child: Text('Page 2'),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              child: const Icon(Icons.keyboard_arrow_left),
+              onPressed: () => controller.previousPage(
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+              ),
+            ),
+            const SizedBox(height: 10),
+            FloatingActionButton(
+              child: const Icon(Icons.keyboard_arrow_right),
+              onPressed: () => controller.nextPage(
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/data_image.dart
+++ lib/utils/data_image.dart
@@ -1,3 +1,5 @@
 const String bg = "assets/images/bg.jpg";
 const String creatures = "assets/images/creatures.png";
 const String logo = "assets/images/logo.png";
+const String areaBootcamp1 = "assets/images/map/area_bootcamp/bg1.jpg";
+const String areaBootcamp2 = "assets/images/map/area_bootcamp/bg2.jpg";
